8602aa5b6cabfb3883e88b42aad611ad48766293
gsaran <gkaur017@protonmail.com>
2024-03-04 00:12:33 -0500

    Implemented search input

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,6 +6,19 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
+const STYLES = {
+  small: {
+    border: '1px',
+    icon: 14,
+    gap: 6
+  },
+  large: {
+    border: '2px',
+    icon: 20,
+    gap: 12
+  }
+}
+
 const IconInput = ({
   label,
   icon,
@@ -13,7 +26,60 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
+  const styles = STYLES[size];
+
+  return (
+    <InputWrapper style={{'--border-width': styles.border, '--gap': `${styles.gap}px`}}>
+      <VisuallyHidden><label for="search-input">{label}</label></VisuallyHidden>
+      <Icon id={icon} size={styles.icon}/>
+      <Input id="search-input" placeholder={placeholder} style={{'--width': `${width}px`}}/>
+    </InputWrapper>
+  );
 };
+
+const Input = styled.input`
+  border: none;
+  width: var(--width);
+  color: ${COLORS.gray700};
+  font-weight: 700;
+  -webkit-appearance: none;
+
+  &::placeholder {
+    font-weight: 400;
+    color: ${COLORS.gray500};
+  }
+
+  &:hover {
+    color: black;
+
+    &::placeholder {
+      font-weight: 400;
+      color: ${COLORS.gray500};
+    }
+  }
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+const InputWrapper = styled.div`
+  border: 1px solid ${COLORS.black};
+  border-width: 0 0 var(--border-width) 0;
+  width: max-content;
+  display: flex;
+  align-items: center;
+  gap: var(--gap);
+  color: ${COLORS.gray700};
+
+  &:has( > ${Input}:hover) {
+    color: black;
+  }
+
+  &:has(> ${Input}:focus) {
+    outline: 1px dotted #212121;
+    outline: 5px auto -webkit-focus-ring-color;
+  }
+`;
 
 export default IconInput;
