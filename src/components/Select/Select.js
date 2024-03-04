03783e073c7178caa813ab6d04647c981f314d97
gsaran <gkaur017@protonmail.com>
2024-03-03 22:05:07 -0500

    Implemented Select using hint from course

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -5,13 +5,54 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import { getDisplayedValue } from './Select.helpers';
 
+
+const SelectWrapper = styled.div`
+  color: ${COLORS.gray700};
+  background-color: ${COLORS.transparentGray15};
+  border: 2px solid transparent;
+  border-radius: 8px;
+  display: flex;
+  align-items: center;
+  gap: 15px;
+  width: fit-content;
+  padding: 4px 8px;
+  position: relative;
+  font-size: 1rem;
+  line-height: 1.5;
+
+  &:hover {
+    color: ${COLORS.black}
+  }
+`;
+
+const StyledSelect = styled.select`
+  /* hint from the course */
+  opacity: 0;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 1;
+  width: 100%;
+  height: 100%;
+
+  /* remove the original arrow */
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  -o-appearance: none;
+`;
+
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <SelectWrapper tabIndex={0}>
+      {displayedValue}
+      <StyledSelect value={value} onChange={onChange}>
+        {children} 
+      </StyledSelect>
+      <Icon id="chevron-down" size={20}/>
+    </SelectWrapper>
   );
 };
 
